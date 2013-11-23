af2a99c7af5845c6dbf8d9b6457c13acfe87bb77
Brad Montgomery <brad@bradmontgomery.net>
2013-11-23 14:55:40 -0600

    embed chosen css directly in the template and add STATIC_URL to any references to the sprite file

--- chosenadmin/middleware.py
+++ chosenadmin/middleware.py
@@ -22,12 +22,20 @@ class ChosenAdminMiddleware(object):
         not_included = self.include_flag not in response.rendered_content
         return correct_path and not_included
 
+    def _minified_css(self):
+        """Read the minified CSS file including STATIC_URL in the references
+        to the sprite images."""
+        css = render_to_string("chosenadmin/css/chosen.min.css", {})
+        for sprite in ['chosen-sprite.png', 'chosen-sprite@2x.png']:
+            css = css.replace(sprite, settings.STATIC_URL + "img/" + sprite)
+        return css
+
     def process_template_response(self, request, response):
 
         if self._match(request, response):
 
-            # Render the <link> and the <script> tags to include Chosen
-            context = {'STATIC_URL': settings.STATIC_URL}
+            # Render the <link> and the <script> tags to include Chosen.
+            context = {"minified_css": self._minified_css()}
             head = render_to_string("chosenadmin/_head_css.html", context)
             body = render_to_string("chosenadmin/_script.html", context)
 
