2797cb8c413574d5b8e3bc3c8c361304cf696dca
Brad Montgomery <brad@bradmontgomery.net>
2017-09-17 14:30:02 -0500

    minor refactor to the middleware

--- chosenadmin/middleware.py
+++ chosenadmin/middleware.py
@@ -12,13 +12,10 @@ class ChosenAdminMiddleware(object):
     def __init__(self, get_response):
         self.get_response = get_response
 
-        self.include_flag = getattr(
-            settings,
-            'CHOSENADMIN_INCLUDE_FLAG',
-            "<!-- CHOSEN INCLUDED -->"
-        )
-        self.minified_css = getattr(settings, 'CHOSENADMIN_MIN_CSS', True)
-        self.minified_js = getattr(settings, 'CHOSENADMIN_MIN_JS', True)
+        self.include_flag = "<!-- CHOSEN INCLUDED -->"
+        self.js_template = "chosenadmin/js/chosen.jquery.min.js"
+        self.css_template = "chosenadmin/css/chosen.min.css"
+        self.chosen_sprites = ['chosen-sprite.png', 'chosen-sprite@2x.png']
 
     def __call__(self, request):
         response = self.get_response(request)
@@ -31,38 +28,36 @@ class ChosenAdminMiddleware(object):
         * The ``include_flag`` is not in the response's content
 
         """
-        correct_path = PATH_MATCHER.match(request.path) is not None
-        not_included = self.include_flag not in response.rendered_content
-        return correct_path and not_included
+        is_html = 'text/html' in response.get('Content-Type', '')
+        if is_html and hasattr(response, 'rendered_content'):
+            correct_path = PATH_MATCHER.match(request.path) is not None
+            not_included = self.include_flag not in response.rendered_content
+            return correct_path and not_included
+        return False
 
-    def _chosen_css(self, minified=True):
+    def _chosen_css(self):
         """Read the minified CSS file including STATIC_URL in the references
         to the sprite images."""
-        file = "chosen.min.css" if minified else "chosen.css"
-        path = "chosenadmin/css/{0}".format(file)
-        css = render_to_string(path, {})
-        for sprite in ['chosen-sprite.png', 'chosen-sprite@2x.png']:
+        css = render_to_string(self.css_template, {})
+        for sprite in self.chosen_sprites:  # rewrite path to sprites in the css
             css = css.replace(sprite, settings.STATIC_URL + "img/" + sprite)
         return css
 
-    def _chosen_js(self, minified=True):
+    def _chosen_js(self):
         """Read the minified jquery plugin file."""
-        file = "chosen.jquery.min.js" if minified else "chosen.jquery.js"
-        path = "chosenadmin/js/{0}".format(file)
-        return render_to_string(path, {})
+        return render_to_string(self.js_template, {})
 
-    def process_template_response(self, request, response):
+    def _embed(self, request, response):
         """Embed Chosen.js directly in html of the response."""
-
         if self._match(request, response):
             # Render the <link> and the <script> tags to include Chosen.
             head = render_to_string(
                 "chosenadmin/_head_css.html",
-                {"minified_css": self._chosen_css(minified=self.minified_css)}
+                {"chosen_css": self._chosen_css()}
             )
             body = render_to_string(
                 "chosenadmin/_script.html",
-                {"chosen_js": self._chosen_js(minified=self.minified_js)}
+                {"chosen_js": self._chosen_js()}
             )
 
             # Re-write the Response's content to include our new html
@@ -70,5 +65,8 @@ class ChosenAdminMiddleware(object):
             content = content.replace('</head>', head)
             content = content.replace('</body>', body)
             response.content = content
-
         return response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return self._embed(request, response)
